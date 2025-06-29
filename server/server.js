3f4e5b922b8eb316a8b98421fe5fadf01400dc46
mansiGhantala <mansighantala@gmailcom>
2025-06-29 12:42:30 +0530

    Updated server and client for deployment

--- server/server.js
+++ server/server.js
@@ -11,8 +11,9 @@ const errorMiddleware = require("./middlewares/error-middleware");
 
 // let's tackle cors 
 const corsOptions = {
-  origin: "http://localhost:5173",
-  methods: "GET, POST, PUT, DELETE, PATCH, HEAD",
+  // origin: "http://localhost:5173",
+  origin: "https://amansolutions.vercel.app",
+  methods:["GET", "POST", "PUT", "DELETE", "PATCH", "HEAD",],
   credentials: true,
 };
 app.use(cors(corsOptions));
