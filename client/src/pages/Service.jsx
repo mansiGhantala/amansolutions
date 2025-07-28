99df574c78c47d4e02786b81bfbb805744b3b7cd
mansiGhantala <mansighantala@gmailcom>
2025-07-28 13:00:41 +0530

    Add line-clamp service descriptions

--- client/src/pages/Service.jsx
+++ client/src/pages/Service.jsx
@@ -1,46 +1,41 @@
-import { useAuth } from "../store/auth"
-import './Service.css'
+import { useAuth } from "../store/auth";
+import './Service.css';
+
 const Service = () => {
+  const { services } = useAuth();
 
-  const {services} = useAuth();
-
-  return (<>
-  <section className="section-services">
-    <div className="container">
-      <h1 className="main-handing">Services</h1>
-    </div>
-    <div className="container grid grid-three-cols">
-
-{services.length === 0 ? (
-    <p>Loading Services...</p>
-) : (
-    services.map((curElem, index) => {
-        const { description, provider, service } = curElem;
-
-        return (
-            <div className="card" key={index}>
-                <div className="card-img">
-                    <img src="/images/about.png" alt="our services info" width='200' />
-                </div>
-                <div className="card-details">
-                    <div className="grid grid-two-cols">
-                        <p>{provider}</p>
+  return (
+    <>
+      <section className="section-services">
+        <div className="container">
+          <h1 className="main-heading">Services</h1>
+        </div>
+        <div className="container grid grid-three-cols">
+          {services.length === 0 ? (
+            <p>Loading Services...</p>
+          ) : (
+            services.map((curElem, index) => {
+              const { description, provider, service } = curElem;
+              return (
+                <div className="card" key={index}>
+                  <div className="card-img">
+                    <img src="/images/about.png" alt="service" />
+                  </div>
+                  <div className="card-details">
+                    <div className="provider-name">
+                      <p>{provider}</p>
                     </div>
                     <h2>{service}</h2>
-                    <p>{description}</p>
+                    <p className="description-text">{description}</p>
+                  </div>
                 </div>
-            </div>
-        );
-    })
-)}
+              );
+            })
+          )}
+        </div>
+      </section>
+    </>
+  );
+};
 
-   
-    </div>
-  </section>
-      
-
-  </>
-  )
-}
-
-export default Service
+export default Service;
